5ba3519b536e11234ceb7eddb445ea4ee0aed5e1
Den Kom <deniss.komarskis@gmail.com>
2025-04-16 22:59:27 +0300

    impr: added movement to boss

---
+++ scenes/entities/enemies/bossold.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+@onready var skin = $NagonfordSkin
+@onready var player = get_node("../Entities/Player")
+
+@export var base_speed := 2.0
+
+var speed_modifier := 1.0
+var player_nearby: bool = true
+
+func _physics_process(delta: float) -> void:
+	move_logic(delta)
+	jump_logic(delta)
+	move_and_slide()
+
+
+func move_logic(delta: float) -> void:
+	var velocity_2d = Vector2(velocity.x, velocity.z)
+
+	if player_nearby:	
+		var direction_to_player = position.direction_to(player.position)
+		var direction_2d = Vector2(direction_to_player.x, direction_to_player.z)
+		var target_angle = -direction_2d.angle() + PI/2
+		skin.rotation.y = rotate_toward(skin.rotation.y, target_angle, 6.0 * delta)
+		velocity_2d += direction_2d * base_speed * delta * 8.0
+		velocity_2d = velocity_2d.limit_length(base_speed) * speed_modifier
+		velocity.x = velocity_2d.x
+		velocity.z = velocity_2d.y
+		print(position.distance_to(player.position))
+		if position.distance_to(player.position) < 2.1:
+			skin.set_move_state("Idle")
+		else:
+			skin.set_move_state("Walking")
+		
+	else:
+		velocity_2d = velocity_2d.move_toward(Vector2.ZERO, base_speed * 4.0 * delta)
+		velocity.x = velocity_2d.x
+		velocity.z = velocity_2d.y
+		skin.set_move_state("Idle")
+		
+func jump_logic(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= 9.8 * delta
--- scenes/entities/enemies/enemy.gd
+++ scenes/entities/enemies/enemy.gd
@@ -1,2 +1,26 @@
-extends CharacterBody3D
 class_name Enemy
+extends CharacterBody3D
+
+@onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var skin = get_node("Skin")
+
+@export var base_speed := 2.0
+@export var notice_radius:= 30
+@export var attack_radius:= 3
+
+func move_to_player(delta):
+	if position.distance_to(player.position) < notice_radius:
+		var target_dir = (player.position - position).normalized()
+		var target_vec2 = Vector2(target_dir.x, target_dir.z)
+		var target_angle = -target_vec2.angle() + PI/2
+		rotation.y = rotate_toward(rotation.y, target_angle, delta * 6.0)
+		if position.distance_to(player.position) > attack_radius:
+			velocity = Vector3(target_vec2.x, 0, target_vec2.y ) * base_speed
+			move_state_machine.travel("walk")
+		else:
+			velocity = Vector3.ZERO
+			move_state_machine.travel("idle")
+		move_and_slide()
+	else:
+		move_state_machine.travel("idle")
